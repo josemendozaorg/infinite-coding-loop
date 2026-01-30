1fdd4fe45878c48055f89ce01ed38e25ac3e2a57
Jose Mendoza <josephrichard7@gmail.com>
2026-01-30 01:06:20 +0100

    refactor: remove code generation and use manual domain types

--- crates/dass-engine/src/domain/mod.rs
+++ crates/dass-engine/src/domain/mod.rs
@@ -1,7 +1,3 @@
 pub mod application;
 // pub mod taxonomy; // Replaced by types (generated)
-#[allow(clippy::clone_on_copy)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[allow(clippy::too_many_arguments)]
-#[allow(clippy::derivable_impls)]
-pub mod types; // Generated types
+pub mod types; // Manual Types
